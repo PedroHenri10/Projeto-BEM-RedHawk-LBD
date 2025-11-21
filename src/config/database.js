5f779a413347cb0e3b9202cff270e749fdc44195
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-11-20 23:48:44 -0300

    config sequelize

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      timestamps: false
+    }
+  }
+);
+
+module.exports = sequelize;
