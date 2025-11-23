0610c236c60fec427a552f09b773f6df1530908b
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-11-23 20:10:14 -0300

    rotas criadas

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
 import userController from "./user.controller.js";
+import auth from "../../middlewares/auth.js"; 
 
 const router = Router();
 
-router.post("/register", userController.register);
+// RF001
+router.post("/register/fisica", userController.registerUser);
+router.post("/register/juridica", userController.registerCompany);
+
+// RF002
 router.post("/login", userController.login);
+
+// RF003
+router.post("/recover-password", userController.recoverPassword);
+
+// RF004
+router.post("/logout", userController.logout);
+
+// RF005 - Protegida por autenticação
+router.put("/profile", auth, userController.updateProfile);
+
+// RF006 - Protegida por autenticação
+router.get("/profile", auth, userController.getProfile);
 
 export default router;
