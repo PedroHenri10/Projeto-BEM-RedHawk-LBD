3ea5f04b580aff8d17cfc3b8cb2028f89b5131ea
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-11-23 19:58:27 -0300

    metodos login e logout controller

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -21,7 +21,48 @@ const userController = {
   },
 
   // RF002
-  
+  login: async (req, res) => {
+    try {
+      const { identifier, senha } = req.body;
+      const { user, accessToken, refreshToken, userType } = await userService.login(identifier, senha);
+      res.status(200).json({ message: "Login realizado com sucesso!", user, accessToken, refreshToken, userType });
+    } catch (error) {
+      res.status(401).json({ error: error.message });
+    }
+  },
+
+ 
+
+  // RF004: O sistema deve permitir que o usuário encerre a sessão.
+  logout: async (req, res) => {
+    try {
+      res.status(200).json({ message: "Sessão encerrada com sucesso." });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  // RF005
+  updateProfile: async (req, res) => {
+    try {
+      const { id, type } = req.user; 
+      const updatedUser = await userService.updateProfile(id, type, req.body);
+      res.status(200).json({ message: "Perfil atualizado com sucesso!", user: updatedUser });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+  // RF006
+  getProfile: async (req, res) => {
+    try {
+      const { id, type } = req.user; 
+      const user = await userService.getProfile(id, type);
+      res.status(200).json({ user });
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  },
 };
 
 export default userController;
