91fb21f297eef758c923e39a7681a3ed25da4232
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-11-28 22:38:31 -0300

    rotas chamadas no app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import express from "express";
 import userRoutes from "./modules/user/user.routes.js";
-import cors from "cors"; 
+import rechargeRoutes from "./modules/recarga/recarga.routes.js"; 
+import cors from "cors";
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/cartao", cardRoutes);     
+app.use("/recarga", rechargeRoutes); 
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Deu ruim!');
+  res.status(500).send('Something broke!');
 });
 
 export default app;
