160c37cedec57d26ce33b56361da49b8b75cd0ab
PEDRO HENRIQUE <dinhonoliver@gmail.com>
2025-11-28 22:13:57 -0300

    add requisito RF015 16 17 controller

--- src/modules/cartao/cartao.controller.js
+++ src/modules/cartao/cartao.controller.js
@@ -37,7 +37,29 @@ const cardController = {
     }
   },
 
-  
+  // RF015
+  getBalances: async (req, res) => {
+    try {
+      const { id, type } = req.user;
+      const balances = await cardService.getCardBalance(id, type);
+      res.status(200).json({ cards: balances });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  // RF016 & RF017
+  getUsageHistory: async (req, res) => {
+    try {
+      const { id, type } = req.user;
+      const { cardId } = req.params;
+      const { orderBy, orderDirection } = req.query; // Ex: ?orderBy=HIS_DATA_HORA&orderDirection=asc
+      const history = await cardService.getCardUsageHistory(cardId, id, type, orderBy, orderDirection);
+      res.status(200).json({ history });
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  },
 };
 
 export default cardController;
